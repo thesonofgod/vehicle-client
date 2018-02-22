0c2f62a3383359814f47575290be07e24c509838
unknown <Miracle@axwaymiracle.ad.miraclesoft.com>
2018-02-22 18:43:52 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+  vinNumber = '';
+  carMake='';
+  carYear='';
+  carRandVinNumber:any;
+  carVin='';
+  myvin:any;
+  randVinNumber = [];
+
+  constructor(private http:Http) {}
+
+
+  
+  searchVin(){
+    this.http.get('http://localhost:8080/api/vehicle/'+this.vinNumber)
+    .subscribe(
+         (res:Response) => {
+          const vehicleRetrieved = res.json();
+          console.log(vehicleRetrieved);
+          this.carMake=vehicleRetrieved.make;
+          this.carYear=vehicleRetrieved.year;
+          this.carVin=vehicleRetrieved.vin;
+      }
+    )
+  }
+
+  randomVin() {
+    this.http.get('http://localhost:8080/api/vehicle')
+    .subscribe(
+    (res:Response) => {
+      this.randVinNumber = res.json();
+      console.log(this.randVinNumber);
+      });
+
+
+      // let vinVal = this.randVinNumber;
+      // const randVinNumber = Math.floor(Math.random()*randVinRetrieval.vin);
+      // let randVinNumber = 
+      // console.log(randVinNumber);
+      // console.log(randVinRetrieval);    
+      // this.carRandVinNumber=randVinNumber;
+      }
+     }
